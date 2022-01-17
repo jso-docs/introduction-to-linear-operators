266e752fd6bd0f97329eaa5530405e6c5bba8c2b
abelsiqueira <abelsiqueira@users.noreply.github.com>
2022-01-17 18:46:17 +0000

    Deploying to gh-preview from  @ ea0bb7d65c5182475227405004c29bd6c1de92d0 🚀

---
+++ previews/PR3/assets/index/code/ex9.jl
@@ -0,0 +1,6 @@
+# This file was generated, do not modify it. # hide
+try
+  Matrix(dft) # ERROR: tried to create a Matrix of Float64
+catch ex
+  println("ex = $ex")
+end
---
+++ previews/PR3/assets/index/code/output/ex16.res
@@ -0,0 +1,2 @@
+1-element Vector{Float64}:
+ 1.6436998861127172
